633d129fe1ac6b23c3542c1c7365b8d464703a75
Juliya Lipatova <575@ukr.net>
2022-09-27 12:19:03 +0300

    added settings

--- fetch-api.js
+++ fetch-api.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q='
 
-const fetchWeather = async function(city = "London"){
+const fetchWeather = async function(city="London" ){
     const apiKey = process.env.SECRET_KEY ;
     const requestUrl = `${BASE_URL}${city}&appid=${apiKey}`
     const response = await fetch(requestUrl);
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const fetchWeather = require('./fetch-api');
 require('dotenv').config()
 
-const app = express()
+const app = express();
+app.use(express.json())
 const PORT = process.env.PORT || 4444
 
 
@@ -25,20 +26,38 @@ res.send(response)
 }
 });
 
-app.get('/get-weather', async(req, res, next)=>{
-   
-    try{
-        const city = req.body;
-        if(!city){
-            return res.json({message: "enter your city"})
-        }
-    
+app.post('/get-weather', async(req, res, next)=>{
+try{
+    const {city} = req.body;
     const response = await fetchWeather(city);
-    res.send(response)
-    }catch(err){
-      next(err)
-    }
+
+    res.status(201).json(response)
+
+}catch(er){
+    next(err)
+}
+
+
     })
+
+    app.use((_, res, __) => {
+        res.status(404).json({
+          status: 'error',
+          code: 404,
+          message: 'Use api on routes: /api/tasks',
+          data: 'Not found',
+        })
+      })
+      
+      app.use((err, _, res, __) => {
+        console.log(err.stack)
+        res.status(500).json({
+          status: 'fail',
+          code: 500,
+          message: err.message,
+          data: 'Internal Server Error',
+        })
+      })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
